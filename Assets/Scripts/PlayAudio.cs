4f2f7ba1ccaf4abe8a814147b728d3df0f320b55
John Patrick Quintos <113108715+patqnts@users.noreply.github.com>
2024-03-16 22:49:48 +0800

    Vowels and consonants canvas layout

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audioSource;
+   public void PlayAudioMethod(AudioClip clips)
+    {
+        audioSource.clip = clips;
+        audioSource.Play();
+    }
+}
